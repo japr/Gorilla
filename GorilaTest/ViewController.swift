45b08bf7345723356014154c0b50614b8334389a
Jorge Palacio <jpalacio19@gmail.com>
2019-11-08 11:55:25 -0500

    Splash screen configured  and initial scene delagate configuration removed

---
+++ GorilaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GorilaTest
+//
+//  Created by Jorge Palacio on 8/11/19.
+//  Copyright © 2019 Jorge Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
